8611b43199627f103218bf061753daed2a813962
Marcin Wosinek <marcin.wosinek@gmail.com>
2022-05-26 17:57:06 +0200

    add module depenency

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,7 @@
-module.exports = {
+import { AngularWebpackPlugin } from "@ngtools/webpack";
+import linkerPlugin from "@angular/compiler-cli/linker/babel";
+
+export default {
   resolve: {
     extensions: [".ts", ".js"],
   },
@@ -8,6 +11,27 @@ module.exports = {
         test: /\.ts$/i,
         use: ["ts-loader"],
       },
+      {
+        test: /\.[jt]sx?$/,
+        loader: "@ngtools/webpack",
+      },
+      {
+        test: /\.[cm]?js$/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            cacheDirectory: true,
+            compact: false,
+            plugins: [linkerPlugin],
+          },
+        },
+      },
     ],
   },
+
+  plugins: [
+    new AngularWebpackPlugin({
+      tsconfig: "./tsconfig.json",
+    }),
+  ],
 };
